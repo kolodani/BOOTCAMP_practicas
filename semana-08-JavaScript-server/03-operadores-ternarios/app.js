79af081ccd2378a9381592d9815ac7c3f81f7d5d
kolodani <danielvinzia@gmail.com>
2022-10-07 16:36:18 -0300

    video de operadores ternarios

---
+++ semana-08-JavaScript-server/03-operadores-ternarios/app.js
@@ -0,0 +1,10 @@
+let patas = 5;
+let accion = "ladra";
+
+// if (patas == 4 && accion == "ladra") {
+//     console.log("Es un perro");
+// }else{
+//     console.log("No es un perro");
+// }
+
+(patas == 4) && (accion == "ladra") ? console.log("Es un perro") : console.log("No es un perro");
