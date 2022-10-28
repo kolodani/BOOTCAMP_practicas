29d5cb2408348dfe8d2c28edac16b68d9e051207
kolodani <danielvinzia@gmail.com>
2022-10-27 21:31:34 -0300

    Segunda parte del video de react state and props

--- semana-11-React/01-react-state-and-props/daniproyect/src/App.js
+++ semana-11-React/01-react-state-and-props/daniproyect/src/App.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 //Import Component
-import Nav from './components/Nav';
-import Tweets from './components/Tweets';
 
 function App() {
   // Write JavaScript code here
+  const [counter, setCounter] = useState(0);
+  const [toggle, setToggle] = useState(false);
+
+  const incrementer = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+  const toggler = () => {
+    setToggle((prev) => !prev);
+  };
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <div className='home'>
-        <Nav />
-        <Tweets />
-      </div>
+      <h1 className={toggle ? "active" : ""}>Hello React</h1>
+      <h2>Counter: {counter}</h2>
+      <button onClick={incrementer}>Click</button>
+      <button onClick={toggler}>Toggle</button>
     </div>
   );
 }
